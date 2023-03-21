8ef943095a8654338d8886c0834d2f8388026701
Wilson <852943830@qq.com>
2023-03-21 17:03:15 +1100

    incremental binnings

---
+++ Discretization/incrementalBinning.py
@@ -0,0 +1,114 @@
+# Using log natural base
+from collections import defaultdict
+from scipy.stats import entropy
+import numpy as np
+
+def entropy_from_counts(counts):
+    n = sum(counts)
+    return np.log(n) - sum(counts*np.log(counts))/n
+
+def incremental_entropy(h_old, n, c_old, c_new):
+    delta = c_new - c_old
+    if n == 0 or n == -delta: # old or new histogram empty
+        return 0.0
+    else:
+        new_term = c_new*np.log(c_new) if c_new > 0 else 0
+        old_term = c_old*np.log(c_old) if c_old > 0 else 0
+        return np.log(n+delta)-(new_term + n*(np.log(n)-h_old) - old_term)/(n+delta)
+    
+class Binning2:
+
+    @staticmethod
+    def trivial(x, y):
+        n, _ = x.shape
+        k = len(np.unique(y))
+        bins = np.zeros(n, dtype=int)
+        max_bin = 0
+        counts =  defaultdict(int) # np.zeros(n, dtype=int)
+        counts[0] = n
+        y_counts = defaultdict(lambda : np.zeros(k, int)) # np.zeros(shape=(n, k), dtype=int)
+        _, y_counts[0] = np.unique(y, return_counts=True)
+        cond_entr = defaultdict(float) # np.zeros(n)
+        cond_entr[0] = entropy(y_counts[0], base=np.e)
+        return Binning2(x, y, bins, max_bin, counts, y_counts, cond_entr, cond_entr[0], 1)
+    
+    @staticmethod
+    def from_assignment(x, y, bins):
+        binning = Binning2.trivial(x, y)
+        for i, dest in enumerate(bins):
+            binning.move(i, dest)
+        return binning
+
+    def __init__(self, x, y, bins, max_bin, counts, y_counts, cond_entr, mean_cond_entr, non_empty_bin_count):
+        n, _ = x.shape
+        self.n = n
+        self.x = x
+        self.y = y
+        self.bins = bins
+        self.max_bin = max_bin
+        self.counts = counts
+        self.y_counts = y_counts
+        self.cond_entr = cond_entr
+        self.mean_cond_entr = mean_cond_entr
+        self.non_empty_bin_count = non_empty_bin_count
+        self.old_mean_cond_entr = mean_cond_entr
+        self.old_non_empty_bin_count = non_empty_bin_count
+
+    def move(self, i, dest):
+        orig = self.bins[i]
+        if orig == dest: 
+            return
+        
+        self.bins[i] = dest
+        c = self.y[i]
+        self.mean_cond_entr = self.mean_cond_entr - self.counts[orig]*self.cond_entr[orig]/self.n - self.counts[dest]*self.cond_entr[dest]/self.n
+        self.counts[orig] -= 1
+        self.counts[dest] += 1
+        self.non_empty_bin_count = self.non_empty_bin_count + (self.counts[dest] == 1) - (self.counts[orig] == 0)
+        self.y_counts[orig][c] -= 1
+        self.y_counts[dest][c] += 1
+        self.cond_entr[orig] = incremental_entropy(self.cond_entr[orig], self.counts[orig]+1, self.y_counts[orig][c]+1, self.y_counts[orig][c])
+        self.cond_entr[dest] = incremental_entropy(self.cond_entr[dest], self.counts[dest]-1, self.y_counts[dest][c]-1, self.y_counts[dest][c])
+        self.mean_cond_entr = self.mean_cond_entr + self.counts[orig]*self.cond_entr[orig]/self.n + self.counts[dest]*self.cond_entr[dest]/self.n
+
+
+    def move_to_cut_off(self, j, split_off_bins):
+        """move real data index j to new bin
+        """
+        b = self.bins[j]
+        if split_off_bins[b] == -1:
+            self.max_bin += 1
+            split_off_bins[b] = self.max_bin
+        _b = split_off_bins[b]
+        return b, _b
+        
+    def apply_cut_off(self, l, order):
+        split_off_bins = np.ones(self.n, dtype=int)*-1
+        for i in range(l+1):
+            j = order[i]
+            _, _b = self.move_to_cut_off(j, split_off_bins)
+            self.move(j, _b)
+
+    def best_cut_off(self, order, obj):
+        _max_bin = self.max_bin
+        split_off_bins = np.ones(self.n, dtype=int)*-1
+        origins = np.zeros(self.n, dtype=int)
+        obj_star = float('inf')
+        i_star = -1
+        # forward
+        for i in range(self.n):
+            j = order[i]
+            b, _b = self.move_to_cut_off(j, split_off_bins)
+            origins[i] = b
+            self.move(j, _b)
+            obj_value = obj(self)
+            if obj_value < obj_star:
+                i_star, obj_star = i, obj_value
+        
+        # rewind
+        for i in range(self.n-1, -1, -1):
+            j = order[i]
+            self.move(j, origins[i])
+        self.max_bin = _max_bin
+
+        return i_star, obj_star
